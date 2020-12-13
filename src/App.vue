42d60f954c4bc928ba016ca5b6628663e3dd26a6
“Guikai <guikai_huang@outlook.com>
2020-12-13 17:02:16 +0800

    logic of header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
 import VHeader from './components/v-header/v-header.vue'
+import { getSeller } from 'api'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      seller: {}
+    }
+  },
   components: {
     VHeader
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
   }
 }
 </script>
